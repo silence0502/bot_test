92e5374abdb7a45d98d2038507530ff665a8bb48
小志 <379658593@qq.com>
2018-03-01 16:23:24 +0800

    v1.0

--- Bot_new.js
+++ Bot_new.js
@@ -7,19 +7,141 @@ db.english_speak_new = new Datastore({ filename: 'db/english_speak_new.db', auto
 class Bot_new extends BaseBot {
     constructor(postData) {
         super(postData);
-        console.log(postData.context.System.user);
-        console.log('this.request.getUserId()', this.request.getUserId());
-        this.addIntentHandler('New_Concept_English', () => {
+        this.addLaunchHandler(() => {
             return {
-                outputSpeech: `
-          1. 学习名曲知识请说：开始学习
-          2. 测试名曲知识请说：开始测试
-          3. 查询曲目作者请说：查询作者
-          4. 查询作者的曲目请说：查询曲目
-          5. 介绍曲目资料请说：介绍曲目
-          `
-            }
+                outputSpeech: '您好，欢迎学习新概念英语!'
+            };
         });
+        this.addIntentHandler('New_Concept_English', () => {
+            var self = this
+
+            var index = self.getSessionAttribute('index')
+
+            var answer = this.getSlot('answer')
+
+            if (answer) {
+                var qs = self.getSessionAttribute('qs')
+                var score = self.getSessionAttribute('score')
+
+                var outputSpeech = ''
+                var result = ''
+                if (answer == qs[index].right) {
+                    console.log('ok');
+                    score++
+                    self.setSessionAttribute('score', score)
+
+                    result = `您选择${answer}，恭喜您答对了！<slience time="3s"></slience>`
+
+                } else {
+                    console.log('fails');
+                    result = `您选择${answer}，抱歉您答错了！ 正确答案是${qs[index].right}.${qs[index].q.answer} 。<slience time="3s"></slience>`
+                }
+                index++
+
+                if (index < 3) {
+                    self.setSessionAttribute('index', index)
+                    outputSpeech = `
+                <speak>
+                ${result}
+                请听下一题
+                ${q[index].q.question}
+                <slience time="5s"></slience>
+                a. <slience time="1s"></slience>${qs[index].a[0]} <slience time="3s"></slience>
+                b. <slience time="1s"></slience>${qs[index].a[1]} <slience time="3s"></slience>
+                c. <slience time="1s"></slience>${qs[index].a[2]} <slience time="3s"></slience>abc您选哪一个？
+                </speak>        
+                `
+                    this.nlu.ask('answer');
+                    return {
+                        outputSpeech: outputSpeech,
+                        reprompt: 'abc您选哪一个？'
+                    }
+                } else {
+
+                    outputSpeech = `
+                <speak>
+                ${result}
+                您在本轮总共答对了${score}个曲目！
+                </speak>        
+                `
+                    this.clearSessionAttribute()
+                    this.endSession()
+                    return {
+                        outputSpeech: outputSpeech,
+                    }
+                }
+
+
+            }
+
+            function getRight(num) {
+                var right = 'a'
+                switch (num) {
+                    case 0:
+                        right = 'a'
+                        break;
+                    case 1:
+                        right = 'b'
+                        break;
+                    case 2:
+                        right = 'c'
+                        break;
+                    default:
+                        break;
+                }
+                console.log('num----------------', right);
+                return right
+            }
+            this.nlu.ask('answer');
+            return new Promise(function (resolve, reject) {
+                db.english_speak_new.find({}, (err, docs) => {
+                    docs = _.shuffle(docs)
+                    var q1_a = _.shuffle([docs[0].answer, docs[docs.length - 1].answer, docs[docs.length - 2].answer])
+                    var q1_right = getRight(_.indexOf(q1_a, docs[0].answer))
+                    var q1 = {
+                        q: docs[0],
+                        right: q1_right,
+                        a: q1_a
+                    }
+
+                    var q2_a = _.shuffle([docs[1].answer, docs[docs.length - 3].answer, docs[docs.length - 4].answer])
+                    var q2_right = getRight(_.indexOf(q2_a, docs[1].answer))
+                    var q2 = {
+                        q: docs[1],
+                        right: q2_right,
+                        a: q2_a
+                    }
+
+                    var q3_a = _.shuffle([docs[2].answer, docs[docs.length - 5].answer, docs[docs.length - 6].answer])
+                    var q3_right = getRight(_.indexOf(q3_a, docs[2].answer))
+                    var q3 = {
+                        q: docs[2],
+                        right: q3_right,
+                        a: q3_a
+                    }
+                    var qs = [q1, q2, q3]
+                    console.log(qs);
+                    self.setSessionAttribute('qs', qs)
+                    self.setSessionAttribute('index', 0)
+                    self.setSessionAttribute('score', 0)
+
+                    resolve({
+                        outputSpeech: `
+                  <speak>
+                  请问以下英文的中文释义是什么？
+                  ${q1.q.question}
+                  <slience time="5s"></slience>
+                  a. <slience time="1s"></slience>${q1.a[0]},<slience time="3s"></slience>
+                  b. <slience time="1s"></slience>${q1.a[1]},<slience time="3s"></slience>
+                  c. <slience time="1s"></slience>${q1.a[2]},<slience time="3s"></slience>abc您选哪一个？
+                  </speak>
+                  `,
+                        reprompt: 'abc您选哪一个？'
+                    })
+
+                })
+            })
+        })
     }
 }
 
